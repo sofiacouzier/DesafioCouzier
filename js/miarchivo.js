04f9fc87e196d831cc58a9b450baf2acf3ad5e62
Sofia Couzier <soficouzier@gmail.com>
2022-07-21 00:09:30 -0300

    desafio Eventos

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -31,3 +31,80 @@ opciones.forEach(Tema => {
 </div>
     `
 })
+
+  
+
+const eleccion = document.getElementById('botonTemas')
+eleccion.addEventListener('click', (event) => {
+    event.preventDefault()
+    let eleccion
+    do{
+        eleccion = prompt('Elija un tema (matematica, historia, arte, deporte, o lengua').toLowerCase()
+        if( eleccion === "matematica" || eleccion === "historia" || eleccion === "arte" || eleccion === "deporte" || eleccion === "lengua"){
+            buscarTema(opciones)
+            break
+        }else{
+           alert ("El tema seleccionado no existe, por favor pruebe otro")
+        }
+    }while ( eleccion != "matematica" || eleccion != "historia" || eleccion != "arte" ||eleccion != "deporte" || eleccion != "lengua")
+     
+
+     function buscarTema(opciones){
+        let opcionElegida = opciones.find( tema => tema.nombre == eleccion)
+        if (opcionElegida == undefined) {
+            console.log('opcion no encontrado')
+        }else{
+            console.log(opcionElegida)
+        }
+ }
+}     )
+     
+   
+   
+
+
+
+
+class Jugador{ 
+    constructor(nombre, email, contraseña) {
+        this.nombre = nombre
+        this.email = email
+        this.contraseña = contraseña
+    }
+}
+
+const Jugadores = []
+
+
+const form = document.getElementById(`idForm`)
+const botonJugadores = document.getElementById(`botonJugadores`)
+const divJugadores = document.getElementById(`divJugadores`)
+
+
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault()
+    let nombre = document.getElementById(`idNombre`).value
+    let email = document.getElementById(`idMail`).value
+    let contraseña = document.getElementById(`idContraseña`).value
+
+    const jugador = new Jugador (nombre, email, contraseña)
+    Jugadores.push(jugador)
+    console.log(Jugadores)
+    form.reset()
+})
+
+botonJugadores.addEventListener(`click`, () => {
+    Jugadores.forEach(jugador => {
+        divJugadores.innerHTML +=
+        `
+        <div class="card" style="width: 18rem; margin: 5px;">
+  
+            <div class="card-body">
+            <h5 class="card-title">${jugador.nombre}</h5>
+            <p class="card-text">${jugador.email}</p>
+    
+            </div>
+</div>
+        `
+    })
+})
